9cdba33e3ce5ebde514243db7d24436e53020935
Aditay <innovationzone32@gmail.com>
2024-12-11 00:09:34 +0530

    commit

--- helper/file.js
+++ helper/file.js
@@ -8,7 +8,7 @@ const s3 = new AWS.S3();
 exports.uploadFile = async (file, filepath, fileName) => {
 	try {
 		const params = {
-			Bucket: 'prince-bucket-1',
+			Bucket: 'uploadre',
 			Key: `${filepath}/${fileName}`,
 			Body: file.buffer,
 			ContentType: file.mimetype,
